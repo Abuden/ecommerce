b6d395ff8f8e5e783ab49cd74fa5128f90914427
Braddy <braddy.yeoh@ucdconnect.ie>
2020-01-17 19:14:26 +0000

    finished website

--- client/src/pages/shop/shop.component.jsx
+++ client/src/pages/shop/shop.component.jsx
@@ -6,8 +6,6 @@ import { fetchCollectionsStart } from '../../redux/shop/shop.actions'
 
 import Spinner from '../../components/spinner/spinner.component'
 
-import { ShopPageContainer } from './shop.styles'
-
 const CollectionsOverviewContainer = lazy(() => import('../../components/collections-overview/collections-overview.container'))
 const CollectionPageContainer = lazy(() => import('../collection/collection.container'))
 
@@ -17,12 +15,12 @@ const ShopPage = ({ fetchCollectionsStart, match }) => {
   }, [fetchCollectionsStart])
 
     return (
-      <ShopPageContainer>
+      <div className='shop-page'>
         <Suspense fallback={<Spinner/>}>
           <Route exact path={`${match.path}`} component={CollectionsOverviewContainer}/>
           <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer}/>
         </Suspense>
-      </ShopPageContainer>
+      </div>
     )
 }
 
